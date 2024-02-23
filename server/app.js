90617ff4a69577ee475762d83634ac7315fb5fc7
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-23 16:56:11 -0600

    add sql connection to app.js

--- server/app.js
+++ server/app.js
@@ -9,3 +9,10 @@ const port = 3001;
 // Middleware
 app.use(express.json());
 
+// MySQL connection
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
