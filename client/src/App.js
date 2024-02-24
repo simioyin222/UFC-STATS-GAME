d2f16616375051a57734ed62235ad35d49e80d56
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-23 21:12:25 -0600

    fetching data from backenbd added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FighterCard from './components/FighterCard';
 
 function App() {
-  // Placeholder for fighter data
-  const fighter = { name: 'John Doe', weight: '170 lbs', height: '6 ft' };
+  const [fighters, setFighters] = useState([]);
+
+  // Fetch fighters from the backend on component mount
+  useEffect(() => {
+    axios.get('http://localhost:3001/fighters')
+      .then(response => {
+        // Update state with fetched fighters
+        setFighters(response.data);
+      })
+      .catch(error => console.error("There was an error fetching the fighters: ", error));
+  }, []);
 
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/">
-            <FighterCard fighter={fighter} />
+            {/* Render a FighterCard for each fighter */}
+            {fighters.map(fighter => (
+              <FighterCard key={fighter.id} fighter={fighter} />
+            ))}
           </Route>
         </Switch>
       </div>
